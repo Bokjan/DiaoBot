4293060756a56a7b9ffbdf7c63ecad43158b3b96
Boyin Chen <i@bokjan.com>
2019-09-07 14:45:09 +0800

    Change: all dynamic linking

---
+++ SDK/include/dll.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#ifdef __cplusplus 
+extern "C" { 
+#endif 
+
+const char * DB_DllName(void);
+void *       DB_GetHandle(void);
+
+#ifdef __cplusplus 
+} 
+#endif 
--- build.sh
+++ build.sh
@@ -2,14 +2,20 @@
 
 set -o errexit
 
+cd `dirname $0`
+SCRIPT_PATH=`pwd`/
+
+#THIRD_BUILDS_PREFIX="/data/builds/"
+
 mkdir -p SDK/build
 mkdir -p Daemon/build
 
 cd SDK/build
 cmake .. -DCMAKE_BUILD_TYPE=RELEASE
 make -j7
+cp libdiaobot.so ../../Dist
 
 cd ../../Daemon/build
-cmake .. -DCMAKE_BUILD_TYPE=RELEASE
+cmake .. -DCMAKE_BUILD_TYPE=RELEASE #-DTHIRD_BUILDS_PREFIX="/data/builds/"
 make -j7
 cp diaobot.out ../../Dist
